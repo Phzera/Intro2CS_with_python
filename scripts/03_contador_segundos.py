4cce64eb1f75a6897739de59305033d2260e3caa
Pedro Oliveira <pedro.oliveira@pitec.co>
2022-08-01 15:47:52 -0300

    Fix alignment

--- scripts/03_contador_segundos.py
+++ scripts/03_contador_segundos.py
@@ -6,9 +6,9 @@
 segundos_str = input("por favor, entre com o número de segundos:\n")
 total_seg    = int(segundos_str)
 
-horas = total_seg // 3600
-seg_restante = total_seg % 3600
-minutos = seg_restante // 60
+horas              = total_seg // 3600
+seg_restante       = total_seg % 3600
+minutos            = seg_restante // 60
 seg_restante_final = seg_restante % 60
 
 print(horas, "horas, ", minutos, "minutos e", seg_restante_final, "segundos.")
